18bfb1ec32946a0f3042d96f6f8d6680122c8b05
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-08 16:06:45 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray (array, element)
+{
+    return array.unshift(element)
+}
+
+
+function destructivelyAddElementToBeginningOfArray (array, element)
+{
+    return array.push(element)
+}
+
+function accessElementInArray (array, index)
+{
+    return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array)
+{
+    return array.shift()
+}
+
+function removeElementFromBeginningOfArray (array)
+{
+    return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+    return arry.pop()
+}
+
+function removeElementFromEndOfArray (array)
+{
+    return array.slice (0, array.length-1)
+}
+
