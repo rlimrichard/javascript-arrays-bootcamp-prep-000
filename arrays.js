72d387f6c6090462ce6fdff4c6799d6fd2fac725
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-08 19:57:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function addElementToBeginningOfArray (array, element)
 
 function destructivelyAddElementToBeginningOfArray (array, element)
 {
-    array.push(element)
+    array.unshift(element)
     return array
 }
 
